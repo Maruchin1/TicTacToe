4fbab348f52676d8c82b78ae25cb42379e6e9334
maruchin <modelarstwo1>
2020-03-04 14:09:00 +0100

    Displaying dynamic board depend on selected board size (currently hardcoded in BoardViewModel)

--- app/src/main/java/com/maruchin/tictactoe/presentation/board/BoardFragment.kt
+++ app/src/main/java/com/maruchin/tictactoe/presentation/board/BoardFragment.kt
@@ -1,30 +1,59 @@
 package com.maruchin.tictactoe.presentation.board
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.maruchin.tictactoe.R
+import com.maruchin.tictactoe.core.entities.PlayerMarker
 import com.maruchin.tictactoe.databinding.FragmentBoardBinding
 import com.maruchin.tictactoe.presentation.framework.BaseFragment
+import kotlinx.android.synthetic.main.fragment_board.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class BoardFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_board) {
 
-    inner class BoardAdapter : BaseAdapter() {
+    private val viewModel: BoardViewModel by viewModel()
 
-        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.boardSize.observe(viewLifecycleOwner, Observer {
+            board_grid.layoutManager = GridLayoutManager(requireContext(), it)
+        })
+        viewModel.fields.observe(viewLifecycleOwner, Observer {
+            board_grid.adapter = BoardAdapter(it)
+        })
+    }
+
+    inner class BoardAdapter(
+        private val fields: List<PlayerMarker>
+    ) : RecyclerView.Adapter<FieldViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FieldViewHolder {
+            val inflater = LayoutInflater.from(requireContext())
+            val view = inflater.inflate(R.layout.view_board_field, parent, false)
+            return FieldViewHolder(view)
         }
 
-        override fun getItem(p0: Int): Any {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun getItemCount(): Int {
+            return fields.size
         }
 
-        override fun getItemId(p0: Int): Long {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+        override fun onBindViewHolder(holder: FieldViewHolder, position: Int) {
 
-        override fun getCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
+    }
+
+    inner class FieldViewHolder(
+        private val itemView: View
+    ) : RecyclerView.ViewHolder(itemView) {
+
     }
 }
--- app/src/main/java/com/maruchin/tictactoe/presentation/board/BoardViewModel.kt
+++ app/src/main/java/com/maruchin/tictactoe/presentation/board/BoardViewModel.kt
@@ -1,10 +1,33 @@
 package com.maruchin.tictactoe.presentation.board
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.maruchin.tictactoe.core.GameService
+import com.maruchin.tictactoe.core.entities.PlayerMarker
 
 class BoardViewModel(
     private val gameService: GameService
 ) : ViewModel() {
 
+    val boardSize: LiveData<Int>
+    val fields: LiveData<List<PlayerMarker>>
+
+    init {
+        boardSize = getBoardSizeLive()
+        fields = getFieldsLive()
+        gameService.startNewGame(boardSize = 3)
+    }
+
+    private fun getBoardSizeLive(): LiveData<Int> {
+        return Transformations.map(gameService.board) {
+            it.size
+        }
+    }
+
+    private fun getFieldsLive(): LiveData<List<PlayerMarker>> {
+        return Transformations.map(gameService.board) {
+            it.fields.flatten()
+        }
+    }
 }
